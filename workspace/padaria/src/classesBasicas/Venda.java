c546af319bd4a5ef1c8950da3dc7666f8a1213dc
Aluno <Aluno>
2017-06-21 17:41:32 -0300

    criacao do repositorio vendas

---
+++ workspace/padaria/src/classesBasicas/Venda.java
@@ -0,0 +1,76 @@
+package classesBasicas;
+
+import java.util.Calendar;
+
+
+public class Venda {
+	
+	// atributos
+	private Calendar    data;
+	private Produto     vendido;
+	private Cliente     comprador;
+	private Funcionario vendedor;
+	
+	
+	// TODO constructor
+	public Venda( Calendar data, Produto vendido, Cliente comprador, Funcionario vendedor ) {
+		
+		this.data = data;
+		this.vendido = vendido;
+		this.comprador = comprador;
+		this.vendedor = vendedor;
+		
+	}
+	
+	
+	// metodos set
+	public void setData( Calendar novaData ) {
+		this.data = novaData;
+	}
+	public void setProduto( Produto novoProduto ) {
+		this.vendido = novoProduto;
+	}
+	public void setComprador( Cliente novoComprador ) {
+		this.comprador = novoComprador;
+	}
+	public void setVendedor( Funcionario novoVendedor ) {
+		this.vendedor = novoVendedor;
+	}
+	
+	
+	// metodos get
+	public Calendar getData() {
+		return this.data;
+	}
+	public Produto getVendido() {
+		return this.vendido;
+	}
+	public Cliente getComprador() {
+		return this.comprador;
+	}
+	public Funcionario getVendedor() {
+		return this.vendedor;
+	}
+	
+	
+	// metodo equals
+	public boolean equals( Venda comparar ) {
+		
+		if( comparar == null ) {
+			return false;
+		}
+		
+		
+		if( this.vendido.equals( comparar.getVendido() ) && this.data.equals( comparar.getData() )  ) {
+			
+			return true;
+			
+		} else {
+			
+			return false;
+			
+		}
+	}
+	
+	
+}
---
+++ workspace/padaria/src/repositorio/RepositorioVendas.java
@@ -0,0 +1,162 @@
+package repositorio;
+
+import java.util.*; // ArrayList
+
+import classesBasicas.*;
+
+public class RepositorioVendas {
+	
+	private ArrayList<Venda> vendas;
+	
+	
+	// construtor
+	public RepositorioVendas( ) {
+		this.vendas = new ArrayList<Venda>();
+	}
+	public RepositorioVendas( ArrayList<Venda> vendas ) {
+		this.vendas = vendas;
+	}
+
+	
+	
+	/*
+	 * TODO ADICIONAR ISTO no cadastro de vendas
+	 * 
+	 * este metodo adiciona uma nova venda no histórico das vendas
+	 * 
+	 * @ parametro data       --- data onde ocorreu a venda
+	 * @ parametro vendido    --- produto que foi vendido
+	 * @ parametro vendedor   --- o funcionario que vendeu o produto
+	 * @ parametro comprador  --- se o cliente foi cadastrado 	->	 opcional
+	 */
+	
+	
+	/*
+	 * este metodo retorna a posicao de uma venda nas vendas
+	 * 
+	 * @ retorna -1 --- se a venda nao foi encontrada
+	 */
+	public int retornarPosicao( Venda procurado ) {
+		
+		Venda aux = null;
+		
+		for( int k = 0; k < this.vendas.size(); k++ ) {
+			
+			
+			aux = this.vendas.get(k);
+			
+			if( aux.equals( procurado ) ) {
+				
+				return k;
+				
+			}
+			
+		}
+		
+		return -1;
+	}
+	
+	
+	/*
+	 * este metodo adiciona uma nova venda no histórico das vendas
+	 * 
+	 * @ parametro venda --- nova venda para adicionar
+	 */
+	public boolean adicionar( Venda venda ) { // sem cliente cadastrado
+		
+		if( venda == null ) {
+			return false;
+		}
+		
+		this.vendas.add( venda );
+		return true;
+	}
+	
+	
+	/*
+	 * este metodo retorna uma array de vendas
+	 * 
+	 * @ retorna Venda[] --- array de vendas
+	 */
+	public Venda[] todos() {
+		
+		if( this.vendas.size() == 0 ) {
+			return null;
+		}
+		
+		Venda[] lista = new Venda[ this.vendas.size() ];
+		
+		for( int k = 0; k < this.vendas.size(); k++ ) {
+			
+			lista[k] = this.vendas.get(k);
+			
+		}
+		
+		return lista;
+	}
+	
+	
+	/*
+	 * este metodo remove uma venda das vendas se a mesma existir
+	 * 
+	 * @ parametro venda --- venda que se deseja remover
+	 */
+	public boolean remover( Venda venda ) {
+		
+		int posicao = retornarPosicao( venda );
+		
+		if( posicao == -1 ) {
+			return false;
+		}
+		
+		
+		this.vendas.remove(posicao);
+		return true;
+	}
+	
+	
+	
+	/*
+	 * este metodo atualiza uma venda existente no estoque por outra
+	 * 
+	 * @ parametro antigo --- venda antiga que sera atualizada
+	 * @ parametro novo   --- nova venda atualizada
+	 */
+	public boolean atualizar( Venda antigo, Venda novo ) {
+		
+		int posicao = this.retornarPosicao( antigo );
+		
+		if( posicao == -1 || novo == null ) {
+			return false;
+		}
+		
+		this.vendas.set(posicao, novo);
+		return true;
+	}
+	
+	
+	
+	/*
+	 * este metodo procura uma venda e retorna ela se a mesma existir
+	 * 
+	 * @ parametro procurado --- venda procurada
+	 */
+	public Venda buscar( Venda procurado ) {
+		
+		int posicao = this.retornarPosicao(procurado);
+		
+		if( posicao == -1 ) {
+			
+			return null;
+			
+		}
+		
+		return this.vendas.get(posicao);
+	}
+
+	
+	
+	
+	
+
+}
