ccea17bab889ddf97f61ccf5ecde129c5d662088
Jonas Freire <jonasfreireperson@gmail.com>
2017-08-15 10:46:02 -0300

    Modificacao no controller do estoque tela funcionario

--- workspace/padaria/src/interfaceGrafica/funcionario/EstoqueFuncionarioLayoutController.java
+++ workspace/padaria/src/interfaceGrafica/funcionario/EstoqueFuncionarioLayoutController.java
@@ -1,12 +1,58 @@
 package interfaceGrafica.funcionario;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import repositorio.*;
+import classesBasicas.*;
+
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 public class EstoqueFuncionarioLayoutController {
 
-	@FXML
-	public void onActionVoltar() {
-		MainFuncionarioTest.setCenaAnterior();
-	}
+    public EstoqueFuncionarioLayoutController(RepositorioProduto produtos) {
+        
+        ObservableList<Produto> dados = FXCollections.observableArrayList( (Produto[])produtos.listar() );
+        
+        tblProduto.setItems( dados );
+        
+        colNome.setCellValueFactory( new PropertyValueFactory<Produto, String>("nome") );
+        
+        
+        
+    }
+    
+    @FXML
+    private TableView<Produto> tblProduto;
+    @FXML
+    private TableColumn<Produto, String> colNome;
+    @FXML
+    private TableColumn<Produto, Double> colPreco;
+    @FXML
+    private TableColumn<Produto, Integer> colQtd;
+    
+    
+    @FXML
+    private Label lblNome;
+    @FXML
+    private Label lblDescricao;
+    @FXML
+    private Label lblValidade;
+    @FXML
+    private Label lblQuantidade;
+    @FXML
+    private Label lblPreco;
+    
+    
+    
+    
+    
+    @FXML
+    public void onActionVoltar() {
+            MainFuncionarioTest.setCenaAnterior();
+    }
 
 }
